807e7ffc160520ecdefe1017a34c82789288d5af
Varuni2021 <81118618+Varuni2021@users.noreply.github.com>
2021-06-23 20:12:53 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var dogImage, happyDog;
+var database;
+var foodS, foodStock;
+
+function preload()
+{
+  dogImage = loadImage("dogImg.png");
+  happyDog = loadImage("dogImg1.png");
+}
+
+function setup() {
+  database = firebase.database();
+
+	createCanvas(500, 500);
+
+  dog = createSprite(250,300,20,20);
+  dog.addImage(dogImage);
+  dog.scale = 0.15;
+
+  foodStock = database.ref("Food");
+  foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+
+  }
+
+  drawSprites();
+
+  fill("white");
+  text("foodStock: " + foodS ,200,200);
+
+  fill("white");
+  text("Press the up arrow key to feed the dog", 150, 50);
+
+
+  //add styles here
+
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }else{
+    x = x-1;
+  }
+  
+database.ref('/').update({
+  Food:x
+
+})
+}
+
+
+
